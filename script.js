cd91ce5fca1aea2a9fad5938f35d81e979fa32cb
Assis Ferreira <ferreira.assisruny@gmail.com>
2021-07-16 18:42:25 -0100

    draw start menu

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert(123);
+var canvas;
+var context;
+
+var menu = "start";
+
+window.onload = function()
+{
+	console.log("starting...");
+
+	canvas = document.querySelector("#screen");
+	context = canvas.getContext("2d");
+
+	update();
+}
+
+function update()
+{
+	window.requestAnimationFrame(update);
+	draw();
+}
+
+function draw()
+{
+	if(menu == "start")
+	{
+		context.fillStyle = "#fff";
+        context.font = "32px Verdana";
+        context.fillText("PingPong", 13, 50);
+	}
+}
